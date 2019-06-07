109d28d5ae6d74ed25bf0790a308a8da8ef6f83b
Freddy Boucher <freddy.boucher@gmail.com>
2019-06-07 16:00:36 +1000

    Enable back Servlet 3 automatic scanning of resources
    This is a following commit of d97ec2dee5fe1a4cde6d42392822a931e9a2ab2b
    
    This is the result of a change introduced in 3.6.2.Final: https://issues.jboss.org/browse/RESTEASY-1748
    The HttpServlet30Dispatcher is registered in the ServletContext with an empty mapping so it conflicts with the welcome-file.
    So the trick is to override the default implementation of HttpServlet30Dispatcher and change:
    
    @WebServlet(asyncSupported = true, value="")
    to
    @WebServlet(asyncSupported = true)
    
    It's a revert of the changes introduced in https://github.com/resteasy/Resteasy/pull/1653/files

--- gwt-storage-objectify-server/src/main/java/com/project/server/OfyHelper.java
+++ gwt-storage-objectify-server/src/main/java/com/project/server/OfyHelper.java
@@ -2,11 +2,15 @@ package com.project.server;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.jboss.resteasy.plugins.server.servlet.HttpServlet30Dispatcher;
 
 import com.googlecode.objectify.ObjectifyService;
 import com.project.shared.entities.GreetingResponse;
 import com.project.shared.entities.User;
 
+@WebListener
 public class OfyHelper implements ServletContextListener {
   public static void register() {
     ObjectifyService.register(User.class);
@@ -20,6 +24,7 @@ public class OfyHelper implements ServletContextListener {
 
   @Override
   public void contextInitialized(ServletContextEvent event) {
+    assert event.getServletContext().getServletRegistration(HttpServlet30Dispatcher.class.getName()) == null;
     // This will be invoked as part of a warmup request, or the first user request if no warmup request was invoked.
     register();
   }
